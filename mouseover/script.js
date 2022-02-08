11ef52cf7424106c711fd03e0b335cb689387bc5
justinwon23 <97702905+justinwon23@users.noreply.github.com>
2022-02-08 08:51:29 -0800

    Add files via upload

---
+++ mouseover/script.js
@@ -0,0 +1,10 @@
+// function add(this){
+//     document.getElementById(this)
+//     backgroundColor = blue;
+
+// }
+
+function add(element) {
+    alert("mouseover");
+    element.style.backgroundColor = "red"
+}

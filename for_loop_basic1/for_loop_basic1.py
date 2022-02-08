e2db114ac14fcc1e2700df20fcf5c549ffc49232
justinwon23 <97702905+justinwon23@users.noreply.github.com>
2022-02-07 21:04:08 -0800

    Add files via upload

---
+++ for_loop_basic1/for_loop_basic1.py
@@ -0,0 +1,28 @@
+for x in range(0,151):
+    print(x)
+
+for x in range(5,1001,5):
+    print(x)
+
+for x in range(1,101):
+    if x % 10 == 0:
+        print('Coding Dojo')
+    elif x % 5 == 0:
+        print('Coding')
+    print(x)
+
+sum = 0
+for x in range(0,500001):
+    if x % 2 != 0:
+        sum = sum + x
+print(sum)
+
+for x in range(2018,-1,-4):
+    print(x)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(2,10,):
+    if x % mult == 0:
+        print(x)
